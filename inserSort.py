dc2eabf79e0d0827fee5c7021d753aff9d055c8c
Anthony Poblacion <StBean@users.noreply.github.com>
2019-03-23 02:25:28 -0700

    Update inserSort.py

--- inserSort.py
+++ inserSort.py
@@ -1,8 +1,9 @@
-def inserSort(data):
+def insertSort(data):
 	for i in range(1, len(data), 1):
-		j = 1
+		j = i
 		while(data[j-1] > data[j] and j > 0):
 			temp = data[j-1]
 			data[j-1] = data[j]
 			data[j] = temp
+			j-=1
 	return data
